127dadfbded18ed7ab91dd73d3cd85791f486389
HackDehZack <Zackseriousemail@gmail.com>
2024-01-16 16:17:56 -0500

    Update database connection configuration

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
 
+
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+
+// create connection to the db
 const sequelize = process.env.JAWSDB_URL
+
+
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
 
 module.exports = sequelize;
