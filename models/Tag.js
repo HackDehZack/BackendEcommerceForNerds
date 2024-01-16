314c3bfd1ad9b49edc6acfd94e954fe384cd90dd
HackDehZack <Zackseriousemail@gmail.com>
2024-01-16 16:12:09 -0500

    Add id and tag_name columns to Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
@@ -16,5 +24,6 @@ Tag.init(
     modelName: 'tag',
   }
 );
+
 
 module.exports = Tag;
